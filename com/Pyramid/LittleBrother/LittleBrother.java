a8e234dfba0aa737fceb56a2999faa1232dfbeb9
kgdwhsk <136995219@qq.com>
2014-08-13 09:34:35 +0800

    Update LittleBrother.java

--- com/Pyramid/LittleBrother/LittleBrother.java
+++ com/Pyramid/LittleBrother/LittleBrother.java
@@ -1,1 +1,24 @@
 //TODO
+package com.Pyramid.LittleBrother;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LittleBrother extends JavaPlugin{
+    
+    /**
+     *
+     */
+    @Override
+    public void onEnable(){ 
+        //for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+        //    playerList.put(player.getName(), playerData(player));
+        //}
+        //getServer().getPluginManager().registerEvents(new MyListener(), this);
+        //getLogger().info("你的插件已被加载！");
+    }
+    @Override
+    public void onDisable(){ 
+        //getLogger().info("你的插件已被卸载。");
+    }
+    
+}
