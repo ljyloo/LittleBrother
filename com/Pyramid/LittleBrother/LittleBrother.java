a5c9f297003b2ae42947f6e58e0578ec8b7f0a41
ljyloo <245158949@qq.com>
2014-08-13 16:49:30 +0800

    Update LittleBrother.java

--- com/Pyramid/LittleBrother/LittleBrother.java
+++ com/Pyramid/LittleBrother/LittleBrother.java
@@ -1,24 +1,9 @@
-//TODO
-package com.Pyramid.LittleBrother;
+package com.ljyloo.LittleBrother;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LittleBrother extends JavaPlugin{
-    
-    /**
-     *
-     */
-    @Override
-    public void onEnable(){ 
-        //for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-        //    playerList.put(player.getName(), playerData(player));
-        //}
-        //getServer().getPluginManager().registerEvents(new MyListener(), this);
-        //getLogger().info("你的插件已被加载！");
-    }
-    @Override
-    public void onDisable(){ 
-        //getLogger().info("你的插件已被卸载。");
-    }
-    
+	public void onEnable(){
+		getLogger().info("LittleBrother插件已被加载！");
+	}
 }
