fddcf87a91101d2991a2bcf460cd19ff68638d4d
kgdwhsk <136995219@qq.com>
2014-08-14 22:15:45 +0800

    Update LittleBrother.java

--- com/Pyramid/LittleBrother/LittleBrother.java
+++ com/Pyramid/LittleBrother/LittleBrother.java
@@ -1,6 +1,8 @@
 package com.Pyramid.LittleBrother;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 public class LittleBrother extends JavaPlugin{
 	private serverThread thread;
@@ -15,4 +17,13 @@ public class LittleBrother extends JavaPlugin{
     	public void onDisable() {
         	getLogger().info("LittleBrother插件已被卸载！");
     	}
+    	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("lb")) { // If the player typed /basic then do the following...
+			// doSomething
+			return true;
+		} //If this has happened the function will return true. 
+        // If this hasn't happened the value of false will be returned.
+		return false; 
+	}
 }
