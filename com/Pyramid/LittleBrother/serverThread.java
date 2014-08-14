28bff284c4e07d16119eaee374a8803386481b60
ljyloo <245158949@qq.com>
2014-08-14 20:42:36 +0800

    Update serverThread.java

--- com/Pyramid/LittleBrother/serverThread.java
+++ com/Pyramid/LittleBrother/serverThread.java
@@ -1,2 +1,17 @@
-//TODO
-//There's nothing here.
+package com.Pyramid.LittleBrother;
+
+import java.util.logging.Logger;
+
+public class serverThread extends Thread{
+	private Logger logger;
+	private server server;
+	
+	public serverThread(Logger logger){
+		this.logger = logger;
+		this.start();
+	}
+	
+	public void run(){
+		this.server = new server(this.logger, 19132, "test");
+	}
+}
