600c8e434775bd47cdaf898b7c937b1f17f8f413
ljyloo <245158949@qq.com>
2014-08-14 20:34:47 +0800

    Update server.java

--- com/Pyramid/LittleBrother/server.java
+++ com/Pyramid/LittleBrother/server.java
@@ -1,1 +1,15 @@
-//TODO
+package com.Pyramid.LittleBrother;
+
+import java.util.logging.Logger;
+
+public class server {
+	private Logger logger;
+	private int port;
+	private String motd;
+	
+	public server(Logger logger, int port, String motd){
+		this.logger = logger;
+		this.port = port;
+		this.motd = motd;
+	}
+}
