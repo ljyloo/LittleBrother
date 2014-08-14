220573e957c1badc1c999c2fc72038b065e7deaa
kgdwhsk <136995219@qq.com>
2014-08-14 22:02:29 +0800

    add onDisable,@Override

--- com/Pyramid/LittleBrother/LittleBrother.java
+++ com/Pyramid/LittleBrother/LittleBrother.java
@@ -5,8 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class LittleBrother extends JavaPlugin{
 	private serverThread thread;
 	
+	@Override
 	public void onEnable(){
 		this.thread = new serverThread(getLogger());
 		getLogger().info("LittleBrother插件已被加载！");
 	}
+	
+	@Override
+    	public void onDisable() {
+        	getLogger().info("LittleBrother插件已被卸载！");
+    	}
 }
