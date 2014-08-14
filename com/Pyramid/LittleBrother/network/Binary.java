157c98f47cf59de4e7df948d5c02de4c2826ef45
ljyloo <245158949@qq.com>
2014-08-14 21:01:33 +0800

    Update Binary.java

--- com/Pyramid/LittleBrother/network/Binary.java
+++ com/Pyramid/LittleBrother/network/Binary.java
@@ -9,24 +9,21 @@ public class Binary{
 		return magic;
 	}
 	
-	public static byte[] getBytes(short data)
-	{
+	public static byte[] getBytes(short data){
 		byte[] bytes = new byte[2];
-        bytes[0] = (byte) (data & 0xff);
-        bytes[1] = (byte) ((data & 0xff00) >> 8);
-        return bytes;
-    }
+        	bytes[0] = (byte) (data & 0xff);
+        	bytes[1] = (byte) ((data & 0xff00) >> 8);
+        	return bytes;
+	}
 
-	public static byte[] getBytes(char data)
-	{
+	public static byte[] getBytes(char data){
 		byte[] bytes = new byte[2];
 		bytes[0] = (byte) (data);
 		bytes[1] = (byte) (data >> 8);
 		return bytes;
 	}
 
-	public static byte[] getBytes(int data)
-	{
+	public static byte[] getBytes(int data){
 		byte[] bytes = new byte[4];
 		bytes[0] = (byte) (data & 0xff);
 		bytes[1] = (byte) ((data & 0xff00) >> 8);
@@ -35,8 +32,7 @@ public class Binary{
 		return bytes;
 	}
 
-	public static byte[] getBytes(long data)
-	{
+	public static byte[] getBytes(long data){
 		byte[] bytes = new byte[8];
 		bytes[0] = (byte) (data & 0xff);
 		bytes[1] = (byte) ((data >> 8) & 0xff);
@@ -49,67 +45,55 @@ public class Binary{
 		return bytes;
 	}
 
-	public static byte[] getBytes(float data)
-	{
+	public static byte[] getBytes(float data){
 		int intBits = Float.floatToIntBits(data);
 		return getBytes(intBits);
 	}
 
-	public static byte[] getBytes(double data)
-	{
+	public static byte[] getBytes(double data){
 		long intBits = Double.doubleToLongBits(data);
 		return getBytes(intBits);
 	}
 
-	public static byte[] getBytes(String data, String charsetName)
-	{
+	public static byte[] getBytes(String data, String charsetName){
 		Charset charset = Charset.forName(charsetName);
 		return data.getBytes(charset);
 	}
 
-	public static byte[] getBytes(String data)
-	{
+	public static byte[] getBytes(String data){
 		return getBytes(data, "UTF8");
 	}
     
-	public static short getShort(byte[] bytes)
-	{
+	public static short getShort(byte[] bytes){
 		return (short) ((0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)));
 	}
 
-	public static char getChar(byte[] bytes)
-	{
+	public static char getChar(byte[] bytes){
 		return (char) ((0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)));
 	}
 
-	public static int getInt(byte[] bytes)
-	{
+	public static int getInt(byte[] bytes){
 		return (0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)) | (0xff0000 & (bytes[2] << 16)) | (0xff000000 & (bytes[3] << 24));
 	}
    
-	public static long getLong(byte[] bytes)
-	{
+	public static long getLong(byte[] bytes){
 		return(0xffL & (long)bytes[0]) | (0xff00L & ((long)bytes[1] << 8)) | (0xff0000L & ((long)bytes[2] << 16)) | (0xff000000L & ((long)bytes[3] << 24)) | (0xff00000000L & ((long)bytes[4] << 32)) | (0xff0000000000L & ((long)bytes[5] << 40)) | (0xff000000000000L & ((long)bytes[6] << 48)) | (0xff00000000000000L & ((long)bytes[7] << 56));
 	}
 
-	public static float getFloat(byte[] bytes)
-	{
+	public static float getFloat(byte[] bytes){
 		return Float.intBitsToFloat(getInt(bytes));
 	}
 
-	public static double getDouble(byte[] bytes)
-	{
+	public static double getDouble(byte[] bytes){
 		long l = getLong(bytes);
 		return Double.longBitsToDouble(l);
 	}
 
-	public static String getString(byte[] bytes, String charsetName)
-	{
+	public static String getString(byte[] bytes, String charsetName){
 		return new String(bytes, Charset.forName(charsetName));
 	}
 
-	public static String getString(byte[] bytes)
-	{
+	public static String getString(byte[] bytes){
 		return getString(bytes, "UTF8");
 	}
 }
