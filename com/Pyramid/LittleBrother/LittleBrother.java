c21e0cd85a751591dfab3beebe915a655a8a358a
ljyloo <245158949@qq.com>
2014-08-14 20:40:18 +0800

    Update LittleBrother.java

--- com/Pyramid/LittleBrother/LittleBrother.java
+++ com/Pyramid/LittleBrother/LittleBrother.java
@@ -3,7 +3,10 @@ package com.Pyramid.LittleBrother;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LittleBrother extends JavaPlugin{
+	private serverThread thread;
+	
 	public void onEnable(){
+		this.thread = new serverThread(getLogger());
 		getLogger().info("LittleBrother插件已被加载！");
 	}
 }
