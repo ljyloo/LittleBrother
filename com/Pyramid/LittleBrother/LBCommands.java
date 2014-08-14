615019b34567e4d9366692b9faa11d94f3a0952f
kgdwhsk <136995219@qq.com>
2014-08-14 22:33:56 +0800

    Update LBCommands.java

--- com/Pyramid/LittleBrother/LBCommands.java
+++ com/Pyramid/LittleBrother/LBCommands.java
@@ -1,12 +1,21 @@
-public class MyPluginCommandExecutor implements CommandExecutor {
+package com.Pyramid.LittleBrother;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class LBCommands implements CommandExecutor {
 	private final LittleBrother plugin;
  
-	public MyPluginCommandExecutor(LittleBrother plugin) {
+	public LBCommands(LittleBrother plugin) {
 		this.plugin = plugin; // Store the plugin in situations where you need it.
 	}
  
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// implementation exactly as before...
+		if (cmd.getName().equalsIgnoreCase("lb")) { 
+			sender.sendMessage("This is LittleBrother Commands list:");
+			return true;
+		}
+		return false;
 	}
 }
