8959e490749ac00afe6df8be9368586191471214
ljyloo <245158949@qq.com>
2014-08-15 21:44:14 +0800

    Update Packet.java

--- com/Pyramid/LittleBrother/network/protocol/Packet.java
+++ com/Pyramid/LittleBrother/network/protocol/Packet.java
@@ -1,23 +1,25 @@
 package com.Pyramid.LittleBrother.network.protocol;
 
+import java.util.ArrayList;
+
 import com.Pyramid.LittleBrother.network.Binary;
 
 abstract class Packet{
-	public int ID = -1;
+	public byte ID = -1;
 	
 	protected int offset = 0;
-	public byte[] buffer;
+	public ArrayList<Byte> buffer = new ArrayList<Byte>();
 	public int sendTime;
 	
 	protected byte[] get(int len){
 		if(len < 0){
-			this.offset = this.buffer.length - 1;
+			this.offset = this.buffer.size() - 1;
 			return null;
 		}
 
 		byte[] buffer = new byte[len];
 		for(int i = 0; len > 0; --len, ++this.offset, ++i){
-			buffer[i] = this.buffer[this.offset];
+			buffer[i] = this.buffer.get(this.offset);
 		}
 
 		return buffer;
@@ -49,12 +51,43 @@ abstract class Packet{
 	}
 	
 	protected boolean feof(){
-		return this.offset > (this.buffer.length - 1);
+		return this.offset > (this.buffer.size() - 1);
 	}
 	
 	protected void put(byte[] bytes){
-		for(int i = 0, n = this.buffer.length; i < bytes.length; i++, n++){
-			
+		for(int i = 0; i < bytes.length; i++){
+			this.buffer.add(bytes[i]);
 		}
+	}
+	
+	protected void putByte(byte b){
+		byte[] bs = new byte[1];
+		bs[0] = b;
+		this.put(bs);
+	}
+	
+	protected void putShort(short s){
+		this.put(Binary.getBytes(s));
+	}
+	
+	protected void putTriad(byte[] t){
+		this.put(t);
+	}
+	
+	protected void putLong(long l){
+		this.put(Binary.getBytes(l));
+	}
+	
+	protected void putString(String s){
+		this.put(Binary.getBytes((short)s.length()));
+		this.put(Binary.getBytes(s));
+	}
+	
+	protected void encode(){
+		this.putByte(this.ID);
+	}
+	
+	protected void decode(){
+		this.offset = 1;
 	}
 }
