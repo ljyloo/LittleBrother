899a2282707c1d6e7f89c6ecb16a7714947fe7b6
MagicDroidX <1323471525@qq.com>
2014-08-15 23:56:51 +0800

    Update LoginPacket.java

--- com/Pyramid/LittleBrother/network/protocol/LoginPacket.java
+++ com/Pyramid/LittleBrother/network/protocol/LoginPacket.java
@@ -1,1 +1,29 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+import java.nio.charset.Charset;
+
+class LoginPacket extends DataPacket{
+	public String username;
+	public int protocol1;
+	public int protocol2;
+	public int clientId;
+	public String loginData;
+	
+	public void pid(){
+	    return Info.LOGIN_PACKET;
+	}
+	
+	public void decode(){
+		this.username = this.getString();
+		this.protocol1 = this.getInt();
+		this.protocol2 = this.getInt();
+		this.clientId = this.getInt();
+		this.loginData = this.getString();
+	}
+	
+	public void encode(){
+
+	}
+	
+}
