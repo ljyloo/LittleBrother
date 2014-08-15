adb515712bee41354521cfc9648f322ea6a3f24f
kgdwhsk <136995219@qq.com>
2014-08-15 18:55:33 +0800

    Update LBCommands.java

--- com/Pyramid/LittleBrother/LBCommands.java
+++ com/Pyramid/LittleBrother/LBCommands.java
@@ -14,13 +14,16 @@ public class LBCommands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("LittleBrother")) { 
 			if (args.length == 0){
-				sender.sendMessage("§a--- 本指令等同于/lb ---");
-				sender.sendMessage("§a/LittleBrother version - §7显示当前版本.");
+				sender.sendMessage("§c--- 本指令等同于/lb ---");
+				sender.sendMessage("§a/LittleBrother version - 显示当前版本.");
+				sender.sendMessage("§a/LittleBrother status - 显示当前运行状态.");
 				return true;
 			} else if ((args.length == 1) && args[0].equalsIgnoreCase("version")) {
 				// version
-				sender.sendMessage(plugin.getDescription().getName() + " §7" + plugin.getDescription().getVersion());
+				sender.sendMessage("§c["+plugin.getDescription().getName() + "-Version] §3" + plugin.getDescription().getVersion());
 				return true;
+			}else if ((args.length == 1) && args[0].equalsIgnoreCase("status")) {
+				sender.sendMessage("§c当前PE服务器名称："+Server.getMotd());
 			}
 		}
 		return false;
