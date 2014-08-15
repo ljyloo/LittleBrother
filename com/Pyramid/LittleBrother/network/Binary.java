a0a53e2cb5316de5837dfab152a1481eac38a5a3
ljyloo <245158949@qq.com>
2014-08-15 22:39:53 +0800

    Update Binary.java

--- com/Pyramid/LittleBrother/network/Binary.java
+++ com/Pyramid/LittleBrother/network/Binary.java
@@ -6,6 +6,14 @@ public class Binary{
 	public static byte PROTOCOL = 0x05;
 	public static byte[] MAGIC = {(byte)0x00, (byte)0xff, (byte)0xff, (byte)0x00, (byte)0xfe, (byte)0xfe, (byte)0xfe, (byte)0xfe, (byte)0xfd, (byte)0xfd, (byte)0xfd, (byte)0xfd, (byte)0x12, (byte)0x34, (byte)0x56, (byte)0x78};
 	
+	public static byte[] getNullPayload(int len){
+		byte[] np = new byte[len];
+		for(int n = 0; n < np.length; n++){
+			np[n] = 0x00;
+		}
+		return np;
+	}
+	
 	public static byte[] getBytes(short data){
 		byte[] bytes = new byte[2];
         	bytes[0] = (byte) (data & 0xff);
