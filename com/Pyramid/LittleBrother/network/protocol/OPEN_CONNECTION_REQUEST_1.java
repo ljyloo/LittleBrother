9b344e81c1fc5ba8da5a15cb512996f147f5a7d1
Steven <qhdghyi@163.com>
2014-08-15 22:26:33 +0800

    Update OPEN_CONNECTION_REQUEST_1.java

--- com/Pyramid/LittleBrother/network/protocol/OPEN_CONNECTION_REQUEST_1.java
+++ com/Pyramid/LittleBrother/network/protocol/OPEN_CONNECTION_REQUEST_1.java
@@ -1,1 +1,30 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class UNCONNECTED_PONG extends Packet{
+
+        public byte ID = 0x05;
+
+        public byte protocol;
+        public mtuSize;
+
+        public void encode(){
+            super.encode();
+            this.put(Binary.getMAGIC());
+            this.putByte(this.protocol);
+            char str = (char)0x00;
+            String fstr;
+            for(i = 1;i <= $this.mtuSize - 18;i++){
+              fstr += str;
+            }
+            this.put(sfstr);
+        }
+        public void decode(){
+            super.decode();
+            this.offset += 16; //magic
+            this.protocol = this.getByte();
+            int n = this.get(true);
+            this.mtuSize = n.length() + 18
+        }
+}
