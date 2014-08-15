40186685231b3b6a4156e782133b95ce0c1f402c
ljyloo <245158949@qq.com>
2014-08-15 22:42:49 +0800

    Update OPEN_CONNECTION_REQUEST_1.java

--- com/Pyramid/LittleBrother/network/protocol/OPEN_CONNECTION_REQUEST_1.java
+++ com/Pyramid/LittleBrother/network/protocol/OPEN_CONNECTION_REQUEST_1.java
@@ -17,7 +17,7 @@ class OPEN_CONNECTION_REQUEST_1 extends Packet{
 	
 	public void decode(){
 		super.decode();
-		this.offset += 16;
+		this.offset += 16; //Magic
 		this.protocol = this.getByte();
 		this.mtuSize = this.getLen();
 	}
