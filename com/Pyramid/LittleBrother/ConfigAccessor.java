47f2b512ee0465fbc51b7c8e63595e4db70a9ec3
kgdwhsk <136995219@qq.com>
2014-08-15 20:45:37 +0800

    Create ConfigAccessor.java

---
+++ com/Pyramid/LittleBrother/ConfigAccessor.java
@@ -0,0 +1,66 @@
+package com.Pyramid.LittleBrother;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+ 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigAccessor {
+ 
+    private final String fileName;
+    private final JavaPlugin plugin;
+    
+    private File configFile;
+    private FileConfiguration fileConfiguration;
+ 
+    public ConfigAccessor(JavaPlugin plugin, String fileName) {
+        if (plugin == null)
+            throw new IllegalArgumentException("plugin cannot be null");
+        this.plugin = plugin;
+        this.fileName = fileName;
+        File dataFolder = plugin.getDataFolder();
+        if (dataFolder == null)
+            throw new IllegalStateException();
+        this.configFile = new File(plugin.getDataFolder(), fileName);
+    }
+ 
+    public void reloadConfig() {        
+        fileConfiguration = YamlConfiguration.loadConfiguration(configFile);
+ 
+        // Look for defaults in the jar
+        InputStream defConfigStream = plugin.getResource(fileName);
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream));
+            fileConfiguration.setDefaults(defConfig);
+        }
+    }
+ 
+    public FileConfiguration getConfig() {
+        if (fileConfiguration == null) {
+            this.reloadConfig();
+        }
+        return fileConfiguration;
+    }
+ 
+    public void saveConfig() {
+        if (fileConfiguration != null && configFile != null) {
+            try {
+                getConfig().save(configFile);
+            } catch (IOException ex) {
+                plugin.getLogger().log(Level.SEVERE, "Could not save config to " + configFile, ex);
+            }
+        }
+    }
+    
+    public void saveDefaultConfig() {
+        if (!configFile.exists()) {            
+            this.plugin.saveResource(fileName, false);
+        }
+    }
+ 
+}
