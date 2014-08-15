b609cb9c295d08b4c2e805ff0024bd6963474409
ljyloo <245158949@qq.com>
2014-08-16 00:19:48 +0800

    Update CLIENT_CONNECT_DataPacket.java

--- com/Pyramid/LittleBrother/network/protocol/CLIENT_CONNECT_DataPacket.java
+++ com/Pyramid/LittleBrother/network/protocol/CLIENT_CONNECT_DataPacket.java
@@ -1,1 +1,23 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+
+class CLIENT_CONNECT_DataPacket extends Packet{
+	public byte ID = 0x09;
+	
+	public long clientID;
+	public long session;
+	public byte unknown;
+	
+	public void encode(){
+		super.encode();
+		this.putLong(clientID);
+		this.putLong(session);
+		this.putByte((byte) 0x00);
+	}
+	
+	public void decode(){
+		super.decode();
+		this.clientID = this.getLong();
+		this.session = this.getLong();
+		this.unknown = this.getByte();
+	}
+}
