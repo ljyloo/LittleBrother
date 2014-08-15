1eb7929fb3391edcf4b68a0e3d67d1bd73b2c6de
ljyloo <245158949@qq.com>
2014-08-15 21:51:45 +0800

    Update UNCONNECTED_PING.java

--- com/Pyramid/LittleBrother/network/protocol/UNCONNECTED_PING.java
+++ com/Pyramid/LittleBrother/network/protocol/UNCONNECTED_PING.java
@@ -1,1 +1,21 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class UNCONNECTED_PING extends Packet{
+	public byte ID = 0x01;
+	
+	public long pingID;
+	
+	public void encode(){
+		super.encode();
+		this.putLong(pingID);
+		this.put(Binary.getMAGIC());
+	}
+	
+	public void decode(){
+		super.decode();
+		this.pingID = this.getLong();
+		//magic
+	}
+}
