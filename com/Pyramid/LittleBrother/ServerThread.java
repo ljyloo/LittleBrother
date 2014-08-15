dd7bcdb740d63d4aaf05a00ed807ff705279dcfb
kgdwhsk <136995219@qq.com>
2014-08-15 21:11:31 +0800

    Update ServerThread.java

--- com/Pyramid/LittleBrother/ServerThread.java
+++ com/Pyramid/LittleBrother/ServerThread.java
@@ -5,13 +5,17 @@ import java.util.logging.Logger;
 public class ServerThread extends Thread{
 	private Logger logger;
 	private Server server;
+	private String Motd;
+	private int port;
 	
 	public ServerThread(Logger logger){
 		this.logger = logger;
+		Motd = LittleBrother.Motd;
+		port = LittleBrother.port;
 		this.start();
 	}
 	
 	public void run(){
-		this.server = new Server(this.logger, 19132, "MCPE Server By LittleBrother");
+		this.server = new Server(this.logger, port, Motd);
 	}
 }
