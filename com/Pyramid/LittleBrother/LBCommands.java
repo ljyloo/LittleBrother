0b8e5644cfb55aedcc35d42a9ffb37571eb7bcdb
kgdwhsk <136995219@qq.com>
2014-08-15 21:32:09 +0800

    Update LBCommands.java

--- com/Pyramid/LittleBrother/LBCommands.java
+++ com/Pyramid/LittleBrother/LBCommands.java
@@ -8,6 +8,7 @@ public class LBCommands implements CommandExecutor {
 	private final LittleBrother plugin;
 	private String Motd;
 	private int port;
+	private ConfigAccessor config = LittleBrother.config;
  
 	public LBCommands(LittleBrother plugin) {
 		this.plugin = plugin; // Store the plugin in situations where you need it.
@@ -19,6 +20,7 @@ public class LBCommands implements CommandExecutor {
 				sender.sendMessage("§c--- 本指令等同于/lb ---");
 				sender.sendMessage("§a/LittleBrother version - 显示当前版本.");
 				sender.sendMessage("§a/LittleBrother status - 显示当前运行状态.");
+				sender.sendMessage("§a/LittleBrother reload - 重载配置文件.");
 				return true;
 			} else if ((args.length == 1) && args[0].equalsIgnoreCase("version")) {
 				// version
@@ -29,6 +31,11 @@ public class LBCommands implements CommandExecutor {
 				port = LittleBrother.port;
 				sender.sendMessage("§c当前PE服务器名称：" + Motd);
 				sender.sendMessage("§c当前PE服务器端口：" + port);
+				return true;
+			} else if ((args.length == 1) && args[0].equalsIgnoreCase("version")) {
+				sender.sendMessage("§c正在重载配置文件！");
+				config.reloadConfig();
+				sender.sendMessage("§c重载配置文件完成！");
 				return true;
 			}
 		}
