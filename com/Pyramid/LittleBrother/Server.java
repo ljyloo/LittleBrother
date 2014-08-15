a045e91edda8aeab5bdb6b32a94aa5b5875372e6
kgdwhsk <136995219@qq.com>
2014-08-15 17:30:52 +0800

    Update Server.java

--- com/Pyramid/LittleBrother/Server.java
+++ com/Pyramid/LittleBrother/Server.java
@@ -2,12 +2,12 @@ package com.Pyramid.LittleBrother;
 
 import java.util.logging.Logger;
 
-public class server {
+public class Server {
 	private Logger logger;
 	private int port;
 	private String motd;
 	
-	public server(Logger logger, int port, String motd){
+	public Server(Logger logger, int port, String motd){
 		this.logger = logger;
 		this.port = port;
 		this.motd = motd;
--- com/Pyramid/LittleBrother/sessionManager.java
+++
@@ -1,2 +0,0 @@
-//TODO
-//There's nothing here.
