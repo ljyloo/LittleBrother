4b0c5c6878780477cd6c28e98f238ad63b540953
ZZM <1013452574@qq.com>
2014-08-17 00:05:21 +0800

    update TileEventPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/TileEventPacket.java
@@ -0,0 +1,30 @@
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class TileEventPacket extends DataPacket{
+	public int x;
+	public int y;
+	public int z;
+	public int case1;
+	public int case2;
+
+	public byte pid(){
+		return Info.TileEventPacket;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(x);
+		this.putInt(y);
+		this.putInt(z);
+		this.putInt(case1);
+		this.putInt(case2);
+		}
+	}
+
+}
