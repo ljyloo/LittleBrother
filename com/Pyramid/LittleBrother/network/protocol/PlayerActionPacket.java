eabdf096f05c8f994d9d0b9c441f3e14ac579e6f
MagicDroidX <1323471525@qq.com>
2014-08-16 23:45:42 +0800

    Update PlayerActionPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/PlayerActionPacket.java
@@ -0,0 +1,28 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class PlayerActionPacket extends DataPacket{
+	public int action;
+	public int x;
+	public int y;
+	public int z;
+	public int face;
+	public int eid;
+
+	public byte pid(){
+		return Info.PLAYER_ACTION_PACKET;
+	}
+
+	public void decode(){
+		action = this.getInt();
+		x = this.getInt();
+		y = this.getInt();
+		z = this.getInt();
+		face = this.getInt();
+		eid = this.getInt();
+	}
+
+	public void encode(){
+
+	}
+
+}
