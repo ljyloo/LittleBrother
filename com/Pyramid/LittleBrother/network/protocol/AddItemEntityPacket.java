49cd5c2991dd097bfa77afa3c51e29d5b4ed7293
MagicDroidX <1323471525@qq.com>
2014-08-16 17:06:02 +0800

    Create AddItemEntityPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/AddItemEntityPacket.java
@@ -0,0 +1,35 @@
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class AddItemEntityPacket extends DataPacket{
+	public int eid;
+	public Item item;
+	public float x;
+	public float y;
+	public float z;
+	public byte yaw;
+	public byte pitch;
+	public byte roll;
+
+	public function pid(){
+		return Info.ADD_ITEM_ENTITY_PACKET;
+	}
+
+	public function decode(){
+
+	}
+
+	public function encode(){
+		this.reset();
+		this.putInt(eid);
+		this.putSlot(item);
+		this.putFloat(x);
+		this.putFloat(y);
+		this.putFloat(z);
+		this.putByte(yaw);
+		this.putByte(pitch);
+		this.putByte(roll);
+	}
+
+}
