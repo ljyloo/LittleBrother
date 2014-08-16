850ded886f544b75b457128bee8e4c97aec6519e
MagicDroidX <1323471525@qq.com>
2014-08-17 00:04:02 +0800

    Update RespawnPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/RespawnPacket.java
@@ -0,0 +1,28 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class RespawnPacket extends DataPacket{
+	public int eid;
+	public float x;
+	public float y;
+	public float z;
+
+	public byte pid(){
+		return Info.RESPAWN_PACKET;
+	}
+
+	public void decode(){
+		eid = this.getInt();
+		x = this.getFloat();
+		y = this.getFloat();
+		z = this.getFloat();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(eid);
+		this.putFloat(x);
+		this.putFloat(y);
+		this.putFloat(z);
+	}
+
+}
