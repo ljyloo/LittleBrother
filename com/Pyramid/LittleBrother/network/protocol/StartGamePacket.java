a7b518c2c37bd3cda423c815f93ccd4fa89667fc
MagicDroidX <1323471525@qq.com>
2014-08-16 15:36:33 +0800

    Update StartGamePacket.java

--- com/Pyramid/LittleBrother/network/protocol/StartGamePacket.java
+++ com/Pyramid/LittleBrother/network/protocol/StartGamePacket.java
@@ -1,1 +1,39 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class StartGamePacket extends DataPacket{
+	public int seed;
+	public int generator;
+	public int gamemode;
+	public int eid;
+	public int spawnX;
+	public int spawnY;
+	public int spawnZ;
+	public float x;
+	public float y;
+	public float z;
+
+	public void pid(){
+		return Info.START_GAME_PACKET;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(seed);
+		this.putInt(generator);
+		this.putInt(gamemode);
+		this.putInt(eid);
+		this.putInt(spawnX);
+		this.putInt(spawnY);
+		this.putInt(spawnZ);
+	  this.putFloat(x);
+		this.putFloat(y);
+		this.putFloat(z);
+	}
+
+}
