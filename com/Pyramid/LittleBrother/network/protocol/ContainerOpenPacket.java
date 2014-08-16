475c3da212fd1c4d7b7fc1badc9b9fe2db2ce3c2
MagicDroidX <1323471525@qq.com>
2014-08-16 23:06:54 +0800

    Update ContainerOpenPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/ContainerOpenPacket.java
@@ -0,0 +1,29 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class ContainerOpenPacket extends DataPacket{
+	public byte windowid;
+	public byte type;
+	public short slots;
+	public int x;
+	public int y;
+	public int z;
+
+	public byte pid(){
+		return Info.CONTAINER_OPEN_PACKET;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putByte(windowid);
+		this.putByte(type);
+		this.putShort(slots);
+		this.putInt(x);
+		this.putInt(y);
+		this.putInt(z);
+	}
+
+}
