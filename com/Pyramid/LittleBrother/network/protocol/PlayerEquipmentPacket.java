bbf4d1c9303051abea183eda1cf384f84fb9df24
MagicDroidX <1323471525@qq.com>
2014-08-16 23:51:24 +0800

    Update

---
+++ com/Pyramid/LittleBrother/network/protocol/PlayerEquipmentPacket.java
@@ -0,0 +1,28 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class PlayerEquipmentPacket extends DataPacket{
+	public int eid;
+	public short item;
+	public short meta;
+	public byte slot;
+
+	public byte pid(){
+		return Info.PLAYER_EQUIPMENT_PACKET;
+	}
+
+	public void decode(){
+		eid = this.getInt();
+		item = this.getShort();
+		meta = this.getShort();
+		slot = this.getByte();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(eid);
+		this.putShort(item);
+		this.putShort(meta);
+		this.putByte(slot);
+	}
+
+}
