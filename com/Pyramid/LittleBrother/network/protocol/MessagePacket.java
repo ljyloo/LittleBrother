595a88d2a6a45dd7c00da17ca7fc363b1480acdc
MagicDroidX <1323471525@qq.com>
2014-08-16 23:40:43 +0800

    Update MessagePacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/MessagePacket.java
@@ -0,0 +1,22 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class MessagePacket extends DataPacket{
+	public String source;
+	public String message;
+
+	public byte pid(){
+		return Info.MESSAGE_PACKET;
+	}
+
+	public void decode(){
+		source = this.getString();
+		message = this.getString();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putString(source);
+		this.putString(message);
+	}
+
+}
