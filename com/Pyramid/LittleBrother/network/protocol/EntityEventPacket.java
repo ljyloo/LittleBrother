414bd95cdaab7aafff003d49881cd9f183ff958b
MagicDroidX <1323471525@qq.com>
2014-08-16 23:30:04 +0800

    Update EntityEventPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/EntityEventPacket.java
@@ -0,0 +1,22 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class EntityEventPacket extends DataPacket{
+	public int eid;
+	public byte event;
+
+	public byte pid(){
+		return Info.ENTITY_EVENT_PACKET;
+	}
+
+	public void decode(){
+		eid = this.getInt();
+		event = this.getByte();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(eid);
+		this.putByte(event);
+	}
+
+}
