12d590631e48f29ea9025f486d65bd35be5ebccc
MagicDroidX <1323471525@qq.com>
2014-08-16 15:32:12 +0800

    Update LoginStatusPacket.java

--- com/Pyramid/LittleBrother/network/protocol/LoginStatusPacket.java
+++ com/Pyramid/LittleBrother/network/protocol/LoginStatusPacket.java
@@ -1,1 +1,21 @@
-//TODO
+package com.Pyramid.LittleBrother.network.protocol;
+import com.Pyramid.LittleBrother.network.Binary;
+import java.nio.charset.Charset;
+
+class LoginStatusPacket extends DataPacket{
+	public int status;
+
+	public void pid(){
+		return Info.LOGIN_STATUS_PACKET;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(this.status);
+	}
+
+}
