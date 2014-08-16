b561122941202ab2fca9c08a7c5a5bfc64e59956
MagicDroidX <1323471525@qq.com>
2014-08-16 23:38:54 +0800

    Update LevelEventPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/LevelEventPacket.java
@@ -0,0 +1,27 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class LevelEventPacket extends DataPacket{
+	public short evid;
+	public int x;
+	public short y;
+	public int z;
+	public int data;
+
+	public byte pid(){
+		return Info.LEVEL_EVENT_PACKET;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putShort(evid);
+		this.putInt(x);
+		this.putShort(y);
+		this.putInt(z);
+		this.putInt(data);
+	}
+
+}
