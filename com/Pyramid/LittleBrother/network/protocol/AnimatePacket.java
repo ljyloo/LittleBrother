ae13f7d997face18995653a4bb87fe60e9c49f5a
MagicDroidX <1323471525@qq.com>
2014-08-16 23:01:39 +0800

    Update AnimatePacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/AnimatePacket.java
@@ -0,0 +1,22 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class AnimatePacket extends DataPacket{
+	public byte action;
+	public int eid;
+
+	public byte pid(){
+		return Info.ANIMATE_PACKET;
+	}
+
+	public void decode(){
+		action = this.getByte();
+		eid = this.getInt();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putByte(action);
+		this.putInt(eid);
+	}
+
+}
