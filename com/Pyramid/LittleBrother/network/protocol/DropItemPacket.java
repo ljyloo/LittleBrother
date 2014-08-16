fa0d1f70d65d8992581be8345e4eeccd16f85535
MagicDroidX <1323471525@qq.com>
2014-08-16 23:26:35 +0800

    Update DropItemPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/DropItemPacket.java
@@ -0,0 +1,22 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class DropItemPacket extends DataPacket{
+	public int eid;
+	public byte unknown;
+	public item item;
+
+	public byte pid(){
+		return Info.DROP_ITEM_PACKET;
+	}
+
+	public void decode(){
+		eid = this.getInt();
+		unknown = this.getByte();
+		item = this.getSlot();
+	}
+
+	public void encode(){
+
+	}
+
+}
