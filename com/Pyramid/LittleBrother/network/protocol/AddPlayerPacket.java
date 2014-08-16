a8a0b6859971149a7ead7a3efe2c3db51ff7e7ec
MagicDroidX <1323471525@qq.com>
2014-08-16 17:33:55 +0800

    Create AddPlayerPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/AddPlayerPacket.java
@@ -0,0 +1,41 @@
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+import java.nio.charset.Charset;
+class AddPlayerPacket extends DataPacket{
+	public long clientID;
+	public String username;
+	public int eid;
+	public float x;
+	public float y;
+	public float z;
+	public float pitch;
+	public float yaw;
+	public Short unknown1;
+	public Short unknown2;
+	public byte[] metadata;
+
+	public function pid(){
+		return Info.ADD_PLAYER_PACKET;
+	}
+
+	public function decode(){
+
+	}
+
+	public function encode(){
+		this.reset();
+		this.putLong(clientID);
+		this.putString(username);
+		this.putInt(eid);
+		this.putFloat(x);
+		this.putFloat(y);
+		this.putFloat(z);
+		//this.putByte((int) (yaw * (256 / 360)));//TODO
+		//this.putByte((int) (pitch * (256 / 360)));//TODO
+		this.putShort(unknown1);
+		this.putShort(unknown2);
+		this.put(Binary.writeMetadata(metadata));
+	}
+
+}
