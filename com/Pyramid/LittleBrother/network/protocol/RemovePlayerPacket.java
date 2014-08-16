378502abab3fc51860f55af91fc72b2ba545589c
MagicDroidX <1323471525@qq.com>
2014-08-16 23:58:58 +0800

    Update RemovePlayerPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/RemovePlayerPacket.java
@@ -0,0 +1,21 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class RemovePlayerPacket extends DataPacket{
+	public int eid;
+	public long clientID;
+
+	public byte pid(){
+		return Info.REMOVE_PLAYER_PACKET;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+		this.reset();
+		this.putInt(eid);
+		this.putLong(clientID);
+	}
+
+}
