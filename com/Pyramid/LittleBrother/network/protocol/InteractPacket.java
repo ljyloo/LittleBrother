d5b084a36ea33855ced6c1693ed84e8919075bc0
MagicDroidX <1323471525@qq.com>
2014-08-16 23:37:08 +0800

    Update InteractPacket.java

---
+++ com/Pyramid/LittleBrother/network/protocol/InteractPacket.java
@@ -0,0 +1,25 @@
+com.Pyramid.LittleBrother.network.protocol;
+
+class InteractPacket extends DataPacket{
+	public byte action;
+	public int eid;
+	public int target;
+
+	public byte pid(){
+		return Info.INTERACT_PACKET;
+	}
+
+	public void decode(){
+		action = this.getByte();
+		eid = this.getInt();
+		target = this.getInt();
+	}
+
+	public void encode(){
+		this.reset();
+		this.putByte(action);
+		this.putInt(eid);
+		this.putInt(target);
+	}
+
+}
