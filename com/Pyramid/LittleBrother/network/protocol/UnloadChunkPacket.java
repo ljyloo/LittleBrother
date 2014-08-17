6d14479dbb8ae094586518e5b21a0fb19ac54bdf
ZZM <1013452574@qq.com>
2014-08-17 10:15:56 +0800

    mdx yoooooooooooooo update UnloadChunkPacket

---
+++ com/Pyramid/LittleBrother/network/protocol/UnloadChunkPacket.java
@@ -0,0 +1,24 @@
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class UnloadChunkPacket extends DataPacket{
+	public int chunkX;
+	public int chunkZ;
+	
+	public byte pid(){
+		return Info.UnloadChunkPacket;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+	    this.reset();
+		this.putInt(chunkX);
+		this.putInt(chunkZ);
+		}
+	}
+
+}
