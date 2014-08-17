842418e20b3925f1e09a0050e40c401e3abeec2a
ZZM <1013452574@qq.com>
2014-08-17 10:16:57 +0800

    mdx yoooooooo Update UpdateBlockPacket

---
+++ com/Pyramid/LittleBrother/network/protocol/UpdateBlockPacket.java
@@ -0,0 +1,30 @@
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.network.Binary;
+
+class UpdateBlockPacket extends DataPacket{
+	public int x;
+	public int z;
+	public byte y;
+	public byte block;
+	public byte meta;
+
+	public byte pid(){
+		return Info.UpdateBlockPacket;
+	}
+
+	public void decode(){
+
+	}
+
+	public void encode(){
+	  this.reset();
+		this.putInt(x);
+		this.putInt(z);
+		this.putByte(y);
+		this.putByte(block);
+		this.putByte(meta);
+		}
+	}
+
+}
