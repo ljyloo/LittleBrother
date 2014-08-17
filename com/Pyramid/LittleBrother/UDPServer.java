da7b65fefb6303bbcc71cb60b3477a15ed41c53c
ljyloo <245158949@qq.com>
2014-08-17 12:54:47 +0800

    Update and rename UDPServerSocket.java to UDPServer.java

--- com/Pyramid/LittleBrother/UDPServer.java
+++ com/Pyramid/LittleBrother/UDPServer.java
@@ -3,25 +3,17 @@ package com.Pyramid.LittleBrother;
 import java.util.logging.Logger;
 import java.net.*;
 
-public class UDPServerSocket {
+public class UDPServer {
     /** @var Logger */
     private logger;
     /** @var DatagramSocket */
     private socket;
     private port;
     
-    public UDPServerSocket(Logger logger, int port) {
+    public UDPServer(Logger logger, int port) {
         this.logger = logger;
         this.port = port;
         
         this.socket = new DatagramSocket(port);
-    }
-    
-    public byte[] readPacket(int length){
-        return new Byte[1];
-    }
-    
-    public int writePacket(byte data[]){
-        return 0;
     }
 }
