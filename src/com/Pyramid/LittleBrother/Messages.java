bf4276e526632bbe7ce9ebe6c66f7862751be1e1
kgdwhsk <136995219@qq.com>
2014-08-18 23:13:28 +0800

    msg

---
+++ src/com/Pyramid/LittleBrother/Messages.java
@@ -0,0 +1,15 @@
+package com.Pyramid.LittleBrother;
+
+public class Messages {
+	//messages
+	public String msgVs = "&a/LittleBrother version - Shows you the version of the plugin.";
+
+	public void loadmsg() {
+		
+	}
+
+	private void savemsg() {
+		
+	}
+
+}
