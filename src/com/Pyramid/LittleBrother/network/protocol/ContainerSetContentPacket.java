2a1c38f8f910ec29d1ba8c0a5a28b20f8a923518
ljy <ljy@ljy-PC>
2014-08-18 15:02:26 +0800

    Fix ContainerSetContentPacket

--- src/com/Pyramid/LittleBrother/network/protocol/ContainerSetContentPacket.java
+++ src/com/Pyramid/LittleBrother/network/protocol/ContainerSetContentPacket.java
@@ -1,44 +1,44 @@
-/*
+package com.Pyramid.LittleBrother.network.protocol;
+
+import com.Pyramid.LittleBrother.Item;
+
 class ContainerSetContentPacket extends DataPacket{
 	public byte windowid;
-	public byte[] slots[];
-	public byte[] hotbar[];
+	public Item slots[];
+	public int hotbar[];
 
 	public byte pid(){
 		return Info.CONTAINER_SET_CONTENT_PACKET;
 	}
 
 	public void decode(){
-		windowid = this.getByte();
-		short count = this.getShort();
-                //slots = new Item[count];
-		for(int s = 0; s < count; ++s){
-			slots[s] = this.getSlot();
-		}
-		if(windowid == 0){
-			count = this.getShort();
-                        //hotbar = new int[count];
-			for(int j = 0; j < count; ++j){
-				hotbar[j] = this.getInt();
-			}
-		}
+        this.windowid = this.getByte();
+        short count = this.getShort();
+        this.slots = new Item[count];
+        for(int s = 0; s < count && !this.feof(); ++s){
+        	this.slots[s] = this.getSlot();
+        }
+        if(this.windowid == 0){
+        	count = this.getShort();
+        	for(int s = 0; s < count && !this.feof(); ++s){
+        		this.hotbar[s] = this.getInt();
+        	}
+        }
 	}
 
 	public void encode(){
 		this.reset();
-		this.putByte(windowid);
-		this.putShort(slots.length);
-		foreach(Item slot : slots){
+		this.putByte(this.windowid);
+		this.putShort((short)this.slots.length);
+		for(Item slot : this.slots){
 			this.putSlot(slot);
 		}
-		if(this.windowid == 0 and this.hotbar.length > 0){
-			this.putShort(this.hotbar.length);
-			foreach(int slot : this.hotbar){
+		if(this.windowid == 0 && this.hotbar.length > 0){
+			this.putShort((short) this.hotbar.length);
+			for(int slot : this.hotbar){
 				this.putInt(slot);
 			}
-		}else{
-			this.putShort(0);
 		}
 	}
-｝
-*/
+
+}
